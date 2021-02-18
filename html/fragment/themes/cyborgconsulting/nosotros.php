2169961085408116a5ba3cf0aeae683faf7c11c8
madvec <m.alejandrodv@gmail.com>
2021-02-18 17:39:40 -0600

    maquetación lista. Pendiente estructura en Fragment

--- html/fragment/themes/cyborgconsulting/nosotros.php
+++ html/fragment/themes/cyborgconsulting/nosotros.php
@@ -33,8 +33,107 @@ include_once "utils/fragment_helpers.php";
             <section class="block us-intro" id="us-intro">
                 <div class="holder">
                     <div class="container-fluid">
-                        <div class="header"></div>
-                        <div class="content"></div>
+                        <div class="header pb-3 pb-md-4">
+                            <div class="row intro-content d-flex align-items-center justify-content-around">
+                                <div class="col-12 col-sm-6">
+                                    <div class="logo text-center">
+                                        <img src="<?= IMGS_PATH ?>logo.svg" alt="logo">
+                                    </div>
+                                </div>
+                                <div class="col-12 col-sm-6">
+                                    <div class="text">
+                                        Somos una consultoría boutique de TI con amplia experiencia en hiperautomatización
+                                        de procesos clave que impulsan a las organizaciones de las diversas industrias a
+                                        alcanzar sus objetivos, mejorando su eficiencia, reduciendo costos y aumentando su
+                                        productividad.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="content pt-3 pt-sm-4">
+                            <div class="row">
+                                <div class="col-6 col-sm-3">
+                                    <div class="wrapper">
+                                        <?php
+                                            $imgNosotros = IMGS_PATH . 'tecnologia-boton.jpg';
+                                            $img_src = sprintf('background-image:url(%s)', $imgNosotros);
+                                        ?>
+                                        <div class="image" style="<?= $img_src ?>"></div>
+                                        <div>
+                                            <div class="logo text-center m-t-30">
+                                                <img class="img-fluid" src="<?= IMGS_PATH ?>icon-chip.svg" alt="chip">
+                                            </div>
+                                            <div class="title normal-size-title text-wrap text-uppercase text-break">Tecnología</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-6 col-sm-3">
+                                    <div class="wrapper">
+                                        <?php
+                                            $imgNosotros = IMGS_PATH . 'consultoria-boton.jpg';
+                                            $img_src = sprintf('background-image:url(%s)', $imgNosotros);
+                                        ?>
+                                        <div class="image" style="<?= $img_src ?>"></div>
+                                        <div>
+                                            <div class="logo text-center m-t-30">
+                                                <img class="img-fluid" src="<?= IMGS_PATH ?>icon-gear.svg" alt="chip">
+                                            </div>
+                                            <div class="title title normal-size-title text-wrap text-uppercase text-break">CONSULTORIA ESPECIALIZADA</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-6 col-sm-3">
+                                    <div class="wrapper">
+                                        <?php
+                                            $imgNosotros = IMGS_PATH . 'implementaciones-boton.jpg';
+                                            $img_src = sprintf('background-image:url(%s)', $imgNosotros);
+                                        ?>
+                                        <div class="image" style="<?= $img_src ?>"></div>
+                                        <div>
+                                            <div class="logo text-center m-t-30">
+                                                <img class="img-fluid" src="<?= IMGS_PATH ?>icon-arm.svg" alt="chip">
+                                            </div>
+                                            <div class="title title normal-size-title text-wrap text-break text-uppercase">
+                                                IMPLEMENTACIONES
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-6 col-sm-3">
+                                    <div class="wrapper">
+                                        <?php
+                                            $imgNosotros = IMGS_PATH . 'operaciones-y-mantenimiento-boton.jpg';
+                                            $img_src = sprintf('background-image:url(%s)', $imgNosotros);
+                                        ?>
+                                        <div class="image" style="<?= $img_src ?>"></div>
+                                        <div>
+                                            <div class="logo text-center m-t-30">
+                                                <img class="img-fluid" src="<?= IMGS_PATH ?>icon-gears.svg" alt="chip">
+                                            </div>
+                                            <div class="title title normal-size-title text-wrap text-uppercase text-break">
+                                            OPERACIONES Y MANTENIMIENTO
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row pt-3 pt-sm-5 intro-content d-flex align-items-start justify-content-around">
+                                <div class="col-12 col-sm-6 pb-4 pb-sm-0">
+                                    <div class="text">
+                                        Acompañamos a nuestros clientes, desde la elaboración del diagnóstico, 
+                                        la generación de alternativas, su evaluación, el diseño de la solución y 
+                                        su instrumentación con la puesta en marcha de cada transformación digital.
+                                    </div>
+                                </div>
+                                <div class="col-12 col-sm-6">
+                                    <div class="text">
+                                        Nuestro equipo de trabajo está altamente capacitado y certificado, 
+                                        lo cual nos permite cumplir con los requerimientos de nuestros clientes, 
+                                        logrando la continuidad de sus negocios.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
