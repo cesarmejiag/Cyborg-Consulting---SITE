e9a990483ddc5866a9a57ae02048ca4e0fba9e23
César Mejía <cesarmejiaguzman@gmail.com>
2021-03-12 13:38:10 -0600

    Create book-ad

--- html/fragment/themes/cyborgconsulting/partials/scripts.php
+++ html/fragment/themes/cyborgconsulting/partials/scripts.php
@@ -1,3 +1,5 @@
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-6046ff77c13ec2bc"></script>
 <!-- jQuery -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <!-- Page Scripts  -->
