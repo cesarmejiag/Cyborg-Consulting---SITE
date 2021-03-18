12f0a1f37997c9f7024a42c00e375e1422275256
César Mejía <cesarmejiaguzman@gmail.com>
2021-03-18 15:36:23 -0600

    Add TODO.md and database

---
+++ TODO.md
@@ -0,0 +1,43 @@
+TODO.md
+========
+
+**Desktop**
+- [x] Cambiar favicon
+- [ ] Texto Cover - cambiar posición de las letras para que no tape el video
+- [ ] Cambiar palabra Hyperautomation
+
+**Nosotros Home**
+- [x] Se baja la s
+- [x] Justificar texto
+- [x] Hablar con Alan para comprar las imágenes
+- [x] Ribbon quitar el adorno naranja
+- [x] Cambiar el color del texto
+- [x] Misión visión y valores
+
+**Nosotros interna**
+- [x] Espacio en Misión y Visión
+- [x] Revisar tipo de texto
+
+**Home móvil**
+- [ ] Revisar video en iPhone
+- [ ] Revisar 
+- [ ] Ebook Agregar espacio
+- [x] Justificar texto
+
+### 18 de marzo 2021
+
+- [ ] Cambiar fuentes
+- [ ] Botón descarga ebook
+- [ ] Nosotros están pegados los botones
+- [ ] Home iconos no alineads
+- [ ] RPA quitar margen
+- [ ] Nuestros Clientes Grupo más y Socio Estrategico
+- [ ] Misión y visión agregar espacio
+- [ ] Enviar cv
+- [ ] Chat
+- [ ] Recortar espacio en servicios entre bloque y bloque
+- [ ] Agregar imágenes en servicios
+- [ ] Agregar formulario
+- [ ] Solicita tu demo
+- [ ] Blog
+- [ ] Slogan poner abajo
--- html/fragment/themes/cyborgconsulting/globals.php
+++ html/fragment/themes/cyborgconsulting/globals.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: cesarmejia
@@ -13,25 +14,25 @@
 /*
  * Site Name
  */
-define("SITE_NAME", "Cyborg Consulting");
+define("SITE_NAME", "CYBORG Consulting | Hiperautomatizacion, RPA e Inteligencia Artificial");
 
 
 /*
  * Site Domain
  */
-define("SITE_DOMAIN", "cyborgconsulting.local");
+define("SITE_DOMAIN", "cyborgconsulting.com.mx");
 
 
 /*
  * Site URL
  */
-define("SITE_URL", sprintf("http://%s", SITE_DOMAIN));
+define("SITE_URL", sprintf("https://%s", SITE_DOMAIN));
 
 
 /*
  * Site Description
  */
-define("SITE_DESCRIPTION", "Cyborg Consulting");
+define("SITE_DESCRIPTION", "Somos una consultoría boutique líder de TI, con amplia experiencia en hiperautomatización, RPA e Inteligencia artificial, con enfoque global.");
 
 
 /*
@@ -43,8 +44,10 @@ define("SITE_SOCIAL_IMAGE", "");
 /*
  * Site Description
  */
-define("SITE_KEYWORDS", implode(", ", array("consulting")));
-
+define("SITE_KEYWORDS", implode(", ", array(
+    "RPA", "Robotizacion", "Inteligencia Artificial", "Tecnologia", "OCR", "ICR", "NLP", "Machine Learning", "Punta a punta", "End to end", "UiPath",
+    "Bots", "Consultoria", "Trasnformacion Digital", "Soluciones", "Chatbot", "Reduccion costos", "Beneficios", "Robots", "Normatividad", "Mejorara de la experiencia del cliente", "Mejora de la experiencia del empleado", "Incremento de productividad", "Trazabilidad", "Escalabilidad", "Digitalizar", "Tareas repetitivas", "Reglas de negocio", "Procesos ", "Entrada estandar", "Analista de negocio", "Arquitecto de solucion", "Tecnico de Infraestructura"
+)));
 
 /*
  * Path that points to site theme.
@@ -55,31 +58,31 @@ define("TEMPLATE_PATH", "/fragment/themes/cyborgconsulting/");
 /*
  * Path that points to css resources of sartoria theme.
  */
-define("CSS_PATH", sprintf("%sdesign/styles/",TEMPLATE_PATH));
+define("CSS_PATH", sprintf("%sdesign/styles/", TEMPLATE_PATH));
 
 
 /*
  * Path that points to img resources of sartoria theme.
  */
-define("IMGS_PATH", sprintf("%sdesign/images/",TEMPLATE_PATH));
+define("IMGS_PATH", sprintf("%sdesign/images/", TEMPLATE_PATH));
 
 
 /*
  * Path that points to js resources of sartoria theme.
  */
-define("JS_PATH", sprintf("%sdesign/scripts/",TEMPLATE_PATH));
+define("JS_PATH", sprintf("%sdesign/scripts/", TEMPLATE_PATH));
 
 
 /*
  * Path that points to favicon resources of sartoria theme.
  */
-define("FAVICONS_PATH", sprintf("%sfavicons/",TEMPLATE_PATH));
+define("FAVICONS_PATH", sprintf("%sfavicons/", TEMPLATE_PATH));
 
 
 /*
  * Path that points to favicon resources of sartoria theme.
  */
-define("FILES_PATH", sprintf("%sfiles/",TEMPLATE_PATH));
+define("FILES_PATH", sprintf("%sfiles/", TEMPLATE_PATH));
 
 
 // ---------------------------------------------------------------------
