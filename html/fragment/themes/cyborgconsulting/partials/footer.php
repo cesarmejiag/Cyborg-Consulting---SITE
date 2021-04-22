35dbfa995235ca9776b70162c3136317c6080649
César Mejía <cesarmejiaguzman@gmail.com>
2021-04-22 15:43:56 -0500

    Se agregó clase sub-menu a las opciones del footer

--- html/fragment/themes/cyborgconsulting/partials/footer.php
+++ html/fragment/themes/cyborgconsulting/partials/footer.php
@@ -8,20 +8,20 @@
         </div>
         <div class="d-none d-sm-block">
             <div class="row">
-                <?php foreach ($root_pages as $page) : ?>
-                    <?php if ($page->guid !== 'home') : ?>
-                        <?php $col_classes = ($page->guid === $us_guid || $page->guid === $services_guid) ? 'col-sm-3 col-md-2' : 'col-sm-3 col-md-1'; ?>
+                <?php foreach ($root_pages as $r_page) : ?>
+                    <?php if ($r_page->guid !== 'home') : ?>
+                        <?php $col_classes = ($r_page->guid === $us_guid || $r_page->guid === $services_guid) ? 'col-sm-3 col-md-2' : 'col-sm-3 col-md-1'; ?>
                         <div class="<?= $col_classes ?>">
                             <div class="list">
-                                <?php if ($page->guid === $rpa_guid) : ?>
+                                <?php if ($r_page->guid === $rpa_guid) : ?>
                                     <h5>RPA</h5>
                                 <?php else : ?>
-                                    <h5><?= $page->title ?></h5>
+                                    <h5><?= $r_page->title ?></h5>
                                 <?php endif; ?>
-                                <?php if ($page->guid === $us_guid || $page->guid === $services_guid || $page->guid === $rpa_guid): ?>
+                                <?php if ($r_page->guid === $us_guid || $r_page->guid === $services_guid || $r_page->guid === $rpa_guid): ?>
                                 <ul>
-                                    <?php foreach ($page->children as $sub_page) : ?>
-                                        <li><a href="<?= $sub_page->key ?>"><?= $sub_page->title ?></a></li>
+                                    <?php foreach ($r_page->children as $sub_page) : ?>
+                                        <li><a class="<?= $page->key === $r_page->key ? 'sub-menu' : '' ?>" href="<?= $r_page->key . '#' . $sub_page->key ?>"><?= $sub_page->title ?></a></li>
                                     <?php endforeach; ?>
                                 </ul>
                                 <?php endif; ?>
