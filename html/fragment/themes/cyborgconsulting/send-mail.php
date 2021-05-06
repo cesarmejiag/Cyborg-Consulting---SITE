d93dce6dac2590e980dddd1ca607b3d02b7c0daa
César Mejía <cesarmejiaguzman@gmail.com>
2021-05-06 13:14:19 -0500

    Change SMTP data from send-mail.php

--- html/fragment/themes/cyborgconsulting/send-mail.php
+++ html/fragment/themes/cyborgconsulting/send-mail.php
@@ -11,6 +11,7 @@ require 'php-mailer/src/SMTP.php';
 
 // Retrieve post data.
 $post = $_POST;
+// $post = array('nombre' => 'César', 'email' => 'cesar@email.com'); // Prueba
 $file = !empty($_FILES['cv']) ? $_FILES['cv'] : false;
 
 //Instantiation and passing `true` enables exceptions
@@ -18,15 +19,16 @@ $mail = new PHPMailer(false);
 
 try {
     //Server settings
-    // $mail->SMTPDebug   = SMTP::DEBUG_SERVER;                    //Enable verbose debug output
+    $mail->SMTPDebug   = SMTP::DEBUG_SERVER;                    //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host        = 'localhost';                           //Set the SMTP server to send through
-    $mail->SMTPAuth    = false;                                 //Enable SMTP authentication
+    $mail->Host        = 'mail.wozit.com.mx';                   //Set the SMTP server to send through
+    $mail->SMTPAuth    = true;                                  //Enable SMTP authentication
     $mail->SMTPAutoTLS = false;
-    $mail->Username    = 'info@cyborgconsulting.com.mx';        //SMTP username
-    $mail->Password    = 'Cyborg2020';                          //SMTP password
-    // $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;      //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port        = 25;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above (default: 587)
+    $mail->Username    = 'prueba@wozit.com.mx';                 //SMTP username
+    $mail->Password    = 'W0z1T1234!';                          //SMTP password
+    $mail->SMTPSecure  = PHPMailer::ENCRYPTION_STARTTLS;        //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    // $mail->SMTPSecure  = PHPMailer::ENCRYPTION_SMTPS;
+    $mail->Port        = 26;                                   //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above (default: 587)
 
     //Recipients
     $mail->setFrom('info@cyborgconsulting.com.mx', 'CyborgConsulting');
@@ -36,7 +38,6 @@ try {
     $mail->addBCC('iconfidence@gmail.com');
     $mail->addBCC('alvaro.algara@gmail.com');
     $mail->addBCC('jessica.blanco@cyborgconsulting.com.mx');
-    $mail->addBCC('alejandro.hernandez@maccomputadoras.com.mx');
 
     //Attachments
     // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
